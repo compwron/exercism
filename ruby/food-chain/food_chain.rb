e9ab55f18f64a0303b68315db67f9239cf1de622
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-18 12:41:00 -0500

    starting over

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,107 +1,25 @@
 class FoodChainSong
-	# Cool solutions:
-	 # http://exercism.io/submissions/bee01e51ef3054e53212a357
-	 # http://exercism.io/submissions/670af96b81477fca83696093
 
 	def verse(num)
-		all_verses = 		{
-		 1 => "#{old_lady_swallowed('fly')}\n#{dunno_why_fly}\n",
-		 2 => "#{old_lady_swallowed('spider')}
-It #{spider_description}.
-#{causes(2)}.
-#{dunno_why_fly}\n",
-      	 3 => "#{old_lady_swallowed('bird')}
-How absurd to swallow a bird!
-#{cause('bird', 'spider')} that #{spider_description}.
-#{cause('spider', 'fly')}.
-#{dunno_why_fly}\n",
-		4 => "#{old_lady_swallowed('cat')}
-Imagine that, to swallow a cat!
-#{cause('cat', 'bird')}.
-#{cause('bird', 'spider')} that #{spider_description}.
-#{cause('spider', 'fly')}.
-#{dunno_why_fly}\n",
-		5 => "#{old_lady_swallowed('dog')}
-What a hog, to swallow a dog!
-#{cause('dog', 'cat')}.
-#{cause('cat', 'bird')}.
-#{cause('bird', 'spider')} that #{spider_description}.
-#{cause('spider', 'fly')}.
-#{dunno_why_fly}\n",
-		6 => "#{old_lady_swallowed('goat')}
-Just opened her throat and swallowed a goat!
-#{cause('goat', 'dog')}.
-#{cause('dog', 'cat')}.
-#{cause('cat', 'bird')}.
-#{cause('bird', 'spider')} that #{spider_description}.
-#{cause('spider', 'fly')}.
-#{dunno_why_fly}\n",
-		7 => "#{old_lady_swallowed('cow')}
-I don't know how she swallowed a cow!
-#{cause('cow', 'goat')}.
-#{cause('goat', 'dog')}.
-#{cause('dog', 'cat')}.
-#{cause('cat', 'bird')}.
-#{cause('bird', 'spider')} that #{spider_description}.
-#{cause('spider', 'fly')}.
-#{dunno_why_fly}\n",
-		8 => "#{old_lady_swallowed('horse')}
-She's dead, of course!\n"
-
-		}
-		all_verses[num] 
-		"#{swallowed(num)}
-		animals[num].phrase"
+		if num == 1 then
+			"I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n"
+		elsif num == 2
+			"I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+  		elsif num == 3
+  			"I know an old lady who swallowed a bird.\n" +
+      "How absurd to swallow a bird!\n" +
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" +
+      "She swallowed the spider to catch the fly.\n" +
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+		end
 	end
 
 	def verses(*nums)
-
-		nums.map {|n| 
-			verse(n)
-		}.join("\n") + "\n"
 	end
 
 	def sing
 		verses(1, 8)
 	end
-
-	def old_lady_swallowed(animal)
-		"I know an old lady who swallowed a #{animal}."
-	end
-
-	def swallowed num
-		old_lady_swallowed(animals.keys[num])
-	end
-
-	def dunno_why_fly
-		
-	end
-
-	def spider_description
-		"wriggled and jiggled and tickled inside her"
-	end
-
-	def cause(animal1, animal2)
-		"She swallowed the #{animal1} to catch the #{animal2}"
-	end
-
-	def animals
-		{'fly' => { :phrase => "I don't know why she swallowed the fly. Perhaps she'll die."}, 
-			'spider' => '', 
-			'bird' => '',
-			'cat' => '',
-			'dog' => '', 
-			'goat' => '', 
-			'cow' => '', 
-			'horse' => ''}
-	end
-	
-	def causes(verse_num)
-		return '' if verse_num > 2
-		verse_num.downto(2).map { |n|
-			"#{cause(animals.keys[n -1], animals.keys[n-2])}"
-		}.join
-	end
-
 end
-
--- ruby/food-chain/food_chain_test.rb
+++ ruby/food-chain/food_chain_test.rb
@@ -35,7 +35,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_cat
-    
+    skip
     expected = "I know an old lady who swallowed a cat.\n" +
       "Imagine that, to swallow a cat!\n" +
       "She swallowed the cat to catch the bird.\n" +
@@ -47,7 +47,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_dog
-    
+    skip
     expected = "I know an old lady who swallowed a dog.\n" +
       "What a hog, to swallow a dog!\n" +
       "She swallowed the dog to catch the cat.\n" +
@@ -60,7 +60,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_goat
-    
+    skip
     expected = "I know an old lady who swallowed a goat.\n" +
       "Just opened her throat and swallowed a goat!\n" +
       "She swallowed the goat to catch the dog.\n" +
@@ -74,7 +74,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_cow
-    
+    skip
     expected = "I know an old lady who swallowed a cow.\n" +
       "I don't know how she swallowed a cow!\n" +
       "She swallowed the cow to catch the goat.\n" +
@@ -89,14 +89,14 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_horse
-    
+    skip
     expected = "I know an old lady who swallowed a horse.\n" +
       "She's dead, of course!\n"
     assert_equal expected, song.verse(8)
   end
 
   def test_multiple_verses
-    
+    skip
     expected = ""
     expected << "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n\n"
     expected << "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" +
@@ -106,7 +106,7 @@ class FoodChainTest < MiniTest::Unit::TestCase
   end
 
   def test_the_whole_song
-    
+    skip
     assert_equal song.verses(1, 8), song.sing
   end
 end
