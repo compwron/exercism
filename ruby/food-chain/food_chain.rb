7fe0f8a1f54edda0e6886f7d2eb8b24d4e1ec836
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-10-18 12:29:12 -0500

    wip

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -4,55 +4,8 @@ class FoodChainSong
 	 # http://exercism.io/submissions/670af96b81477fca83696093
 
 	def verse(num)
-		all_verses[num] 
-	end
-
-	def verses(*nums)
-		nums.map {|n| 
-			verse(n)
-		}.join("\n") + "\n"
-	end
-
-	def sing
-		verses(1, 8)
-	end
-
-	def old_lady_swallowed(animal)
-		"I know an old lady who swallowed a #{animal}."
-	end
-
-	def dunno_why_fly
-		"I don't know why she swallowed the fly. Perhaps she'll die."
-	end
-
-	def spider_description
-		"wriggled and jiggled and tickled inside her"
-	end
-
-	def cause(animal1, animal2)
-		"She swallowed the #{animal1} to catch the #{animal2}"
-	end
-
-	def animals
-		{'fly' => '', 
-			'spider' => '', 
-			'bird' => '',
-			'cat' => '',
-			'dog' => '', 
-			'goat' => '', 
-			'cow' => '', 
-			'horse' => ''}
-	end
-	
-	def causes(verse_num)
-		return '' if verse_num > 2
-		verse_num.downto(2).map { |n|
-			"#{cause(animals.keys[n -1], animals.keys[n-2])}"
-		}.join
-	end
-
-	def all_verses
-		{1 => "#{old_lady_swallowed('fly')}\n#{dunno_why_fly}\n",
+		all_verses = 		{
+		 1 => "#{old_lady_swallowed('fly')}\n#{dunno_why_fly}\n",
 		 2 => "#{old_lady_swallowed('spider')}
 It #{spider_description}.
 #{causes(2)}.
@@ -96,6 +49,59 @@ I don't know how she swallowed a cow!
 She's dead, of course!\n"
 
 		}
+		all_verses[num] 
+		"#{swallowed(num)}
+		animals[num].phrase"
 	end
+
+	def verses(*nums)
+
+		nums.map {|n| 
+			verse(n)
+		}.join("\n") + "\n"
+	end
+
+	def sing
+		verses(1, 8)
+	end
+
+	def old_lady_swallowed(animal)
+		"I know an old lady who swallowed a #{animal}."
+	end
+
+	def swallowed num
+		old_lady_swallowed(animals.keys[num])
+	end
+
+	def dunno_why_fly
+		
+	end
+
+	def spider_description
+		"wriggled and jiggled and tickled inside her"
+	end
+
+	def cause(animal1, animal2)
+		"She swallowed the #{animal1} to catch the #{animal2}"
+	end
+
+	def animals
+		{'fly' => { :phrase => "I don't know why she swallowed the fly. Perhaps she'll die."}, 
+			'spider' => '', 
+			'bird' => '',
+			'cat' => '',
+			'dog' => '', 
+			'goat' => '', 
+			'cow' => '', 
+			'horse' => ''}
+	end
+	
+	def causes(verse_num)
+		return '' if verse_num > 2
+		verse_num.downto(2).map { |n|
+			"#{cause(animals.keys[n -1], animals.keys[n-2])}"
+		}.join
+	end
+
 end
 
