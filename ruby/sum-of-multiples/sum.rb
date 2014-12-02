8c95f8587131e8db9f54316fa941bfbb2918e1d9
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-12-02 10:30:47 -0800

    WIP gradeschool

--- ruby/sum-of-multiples/sum.rb
+++ ruby/sum-of-multiples/sum.rb
@@ -1,25 +1,29 @@
 class SumOfMultiples
 	DEFAULT_DIVISORS = [3, 5]
 
-	def initialize *nums
+	def initialize *nums = [3, 5]
 		@divisors = *nums
 	end
 
 	def to num
-		Foo.foo num, @divisors
-	end
-
-	def self.to num
-		Foo.foo num, DEFAULT_DIVISORS
-	end
-end
-
-class Foo 
-	def self.foo num, divisors
-	1.upto(num -1).select {|n|
+		1.upto(num -1).select {|n|
 			divisors.map {|d|
 				n % d == 0
 			}.any?
 		}.inject(&:+) || 0
 	end
+
+	def self.to num
+		# Foo.foo num, DEFAULT_DIVISORS
+		new(DEFAULT_DIVISORS).to num
+	end
 end
+
+# class Foo 
+# 	def self.foo num, divisors
+	
+# end
+
+ # def self.to(limit)
+ #    new(3, 5).to(limit)
+ #  end
