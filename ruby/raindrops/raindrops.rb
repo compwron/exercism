8da2cb5586dca41ba18e51842fa22492b0c035c9
Linda Goldstein <linda.goldstein@getbraintree.com>
2015-03-04 22:39:15 -0800

    mess

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -3,6 +3,8 @@ class Raindrops
 	@mappings = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
 
 	def self.convert(number)
+		@mappings.each_with_object([])
+		
 		drops = @mappings.map { |prime, word|
 			number.modulo(prime).zero? ? word : ''
 		}.join
---
+++ ruby/space-age/space_age.rb
@@ -0,0 +1,8 @@
+class SpaceAge
+	attr_reader :seconds
+
+	@planets = {:earth => 31557600, :mercury => }
+	def initialize num
+		@seconds = num
+	end
+end
